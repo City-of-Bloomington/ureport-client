c98f7981ca5b667c1d6868e1db0483a1a1a29c37
Cliff Ingham <inghamn@bloomington.in.gov>
2024-02-29 16:09:14 -0500

    Implemented file size checking
    Use javascript to check if the chosen file is too big to upload.

---
+++ public/js/form.js
@@ -0,0 +1,16 @@
+"use strict";
+function validate() {
+    const media = document.getElementById('media'),
+          files = media.files,
+          max   = 10 * 1024 * 1024;
+    if (files.length > 0) {
+        if (files[0].size > max) {
+            media.setCustomValidity('The image must not be larger than 10MB');
+            media.reportValidity();
+            return false;
+        }
+    }
+    media.setCustomValidity('');
+    return true;
+}
+document.getElementById('media').onchange = validate;
